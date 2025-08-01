0c08cb0afd88e31c8cac8c0bf2ba5be1f77931b8
gbmao <gabriel.machado.oli@gmail.com>
2025-07-31 21:42:33 -0300

    wip: add a way to create enemies

--- src/characters/Enemies.java
+++ src/characters/Enemies.java
@@ -1,4 +1,34 @@
 package characters;
 
 public class Enemies {
+    private String enemyType;
+    private int health;
+    private int atackDamage;
+
+    public Enemies(String enemyType, int health,int atackDamage){
+        this.enemyType = enemyType;
+        this.health = health;
+        this.atackDamage = atackDamage;
+    }
+
+    public String GetEnemyType(){ return enemyType; }
+    public int GetHealth(){return health;}
+    public int GetAtackDamage(){ return atackDamage;}
+
+    public void setEnemyType(String enemyType) {
+        this.enemyType = enemyType;
+    }
+
+    public void setAtackDamage(int atackDamage) {
+        this.atackDamage = atackDamage;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    // Enemies() {enemyType = "Slime"; health = 4; atackDamage = 1;};
+
+
+
 }
