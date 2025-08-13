99df19e35371b8e7d8234eb977bcd86405c59ecc
gbmao <gabriel.machado.oli@gmail.com>
2025-08-13 14:27:40 -0300

    feat: new method to create a text inside de box

--- src/interfaces/Menu.java
+++ src/interfaces/Menu.java
@@ -17,22 +17,30 @@ public class Menu {
                     //print lateral border
                     if (column == 1 || column == 100) {
 
-                        // metodo de digitar algo
-                        if(column == 1 && row == 20){
-                            System.out.print("| ");
-                            System.out.printf("%-98s", "testando o metodo de digitao"); // cuidado para nao consumir espaco alem do possivel
-                        }else
-                        System.out.printf("%-100s", barra);
+                        // metodo de digitar  algo: rowText deve ser inserido aqui
+                        // inserido dentro de if para evitar repetir a barra 2x
+                        if (!rowText(column, row, 20)) {
+                            System.out.printf("%-100s", barra); // ajustar tamanho para se adequar a uma nova criacao de uma barra lateral
+                        }
                     }
 
-//                    else
-//                        System.out.print(" ");
             }
 
             System.out.println();
 
         }
 
+    }
+    //boolean para evitar repeticao com o codigo que cria as barras laterais.
+    // algum metodo melhor???
+    public static boolean rowText(int column, int row, int rowTarget) {
+
+        if (column == 1 && row == rowTarget) {
+            System.out.print("| ");
+            System.out.printf("%-98s", "testando o metodo de digitao"); // cuidado para nao consumir espaco alem do possivel
+            return true;
+        }
+        return false;
     }
 
 
