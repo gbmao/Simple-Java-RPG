3b52974a199cacda632bc514de0929367d925504
gbmao <gabriel.machado.oli@gmail.com>
2025-08-13 17:06:31 -0300

    feat: feed some new enemies

--- src/database/GameEnemies.java
+++ src/database/GameEnemies.java
@@ -5,4 +5,7 @@ import characters.Enemies;
 public class GameEnemies {
 
     public static Enemies slime = new Enemies("Slime", 5, 1);
+    public static Enemies skeletonWarrior = new Enemies("Skeleton Warrior", 7, 3);
+    public static Enemies skeletonArcher = new Enemies("Skeleton Archer", 4, 4);
+    public static Enemies firstBoss = new Enemies("Giant Spider", 12, 4);
 }
