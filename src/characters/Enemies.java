c614ec2fcd618cb4f2de971b74f998a9f6f79f9d
gbmao <gabriel.machado.oli@gmail.com>
2025-08-18 21:15:29 -0300

    refactoring

--- src/characters/Enemies.java
+++ src/characters/Enemies.java
@@ -3,33 +3,48 @@ package characters;
 public class Enemies {
     private String enemyType;
     private int health;
-    private int atackDamage;
+    private int attackDamage;
     private int xp;
 
-    public Enemies(String enemyType, int health,int atackDamage, int xp){
+    public Enemies() {
+    }
+
+    public Enemies(String enemyType, int health, int attackDamage, int xp) {
         this.enemyType = enemyType;
         this.health = health;
-        this.atackDamage = atackDamage;
+        this.attackDamage = attackDamage;
         this.xp = xp;
     }
 
-    public String GetEnemyType(){ return enemyType; }
-    public int GetHealth(){return health;}
-    public int GetAtackDamage(){ return atackDamage;}
+    public String GetEnemyType() {
+        return enemyType;
+    }
+
+    public int GetHealth() {
+        return health;
+    }
+
+    public int GetAttackDamage() {
+        return attackDamage;
+    }
+
+    public int getXp() {
+        return xp;
+    }
 
     public void setEnemyType(String enemyType) {
         this.enemyType = enemyType;
     }
 
-    public void setAtackDamage(int atackDamage) {
-        this.atackDamage = atackDamage;
+    public void setAttackDamage(int atackDamage) {
+        this.attackDamage = atackDamage;
     }
 
     public void setHealth(int health) {
         this.health = health;
     }
 
-    public void EnemyAtack(){
+    public void EnemyAttack() {
 
 
     }
