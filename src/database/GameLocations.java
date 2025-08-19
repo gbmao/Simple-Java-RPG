a99ad04b49dc4ba5642ab118d6cbcb6687bf2ba9
gbmao <gabriel.machado.oli@gmail.com>
2025-08-18 21:04:46 -0300

    feat: new method for calling room description from stance

--- src/database/GameLocations.java
+++ src/database/GameLocations.java
@@ -6,34 +6,37 @@ import utils.Utils;
 public class GameLocations {
 
 
-//    public static Locations actualRoom = new Locations(name(), light(), size());
 
-    public static String roomDescription(){
+    public static String roomDescription() {
 
-        return name() +size()+ light();
+        Locations room = new Locations(name(), light(), size());
+        return "You entered an " + room.getName() + room.getLight() + room.getSize();
     }
 
-    public static String size(){
 
-        return switch(Utils.RNG(1, 3)){
+    public static String size() {
+
+        return switch (Utils.RNG(1, 3)) {
             case 1 -> " big room";
             case 2 -> " small room";
             case 3 -> " strange room";
             default -> " room";
         };
     }
-    public static String light(){
 
-        return switch(Utils.RNG(1, 3)){
+    public static String light() {
+
+        return switch (Utils.RNG(1, 3)) {
             case 1 -> " with the sun light coming from a little window";
             case 2 -> " with a simple torch lighting the place.";
             case 3 -> " with some lifted candles";
             default -> " with no light source";
         };
     }
-    public static String name(){
 
-        return switch(Utils.RNG(1, 3)){
+    public static String name() {
+
+        return switch (Utils.RNG(1, 3)) {
             case 1 -> " strange";
             case 2 -> " mysterious";
             case 3 -> " golden";
--- src/locations/Locations.java
+++ src/locations/Locations.java
@@ -4,17 +4,19 @@ public class Locations {
     private String name;
     private String light;
     private String size;
+    private static int totalRooms;
 
     public Locations(String name, String light, String size){
     this.name = name;
     this.light = light;
     this.size = size;
+    totalRooms++;
 
     }
 
-    public String GetName(){ return name;}
-    public String GetLight(){return light;}
-    public String GetSize(){ return size;}
+    public String getName(){ return name;}
+    public String getLight(){return light;}
+    public String getSize(){ return size;}
 
     public void setName(String name){
         this.name =name;
