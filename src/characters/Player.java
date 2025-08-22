fb2b8d9c814181a1d3413352b0c1a01f58029ff7
gbmao <gabriel.machado.oli@gmail.com>
2025-08-21 21:57:04 -0300

    feat: tookDAmage add

--- src/characters/Player.java
+++ src/characters/Player.java
@@ -9,18 +9,22 @@ public class Player {
     private int health;
     private int damage;
     private int xp;
+    protected boolean isAlive;
 
-    public Player(){
-        this("AAA", 0, 10, 2, 0);
+    public static Player player1 = new Player("AAA", 1, 10, 2, 0);
+
+    public Player() {
+//        this("AAA", 1, 10, 2, 0);
     }
 
-     public Player(String name, int level, int health, int damage, int xp){
-         this.name = name;
-         this.level = level;
-         this.health = health;
-         this.damage = damage;
-         this.xp = xp;
-     }
+    public Player(String name, int level, int health, int damage, int xp) {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+        this.damage = damage;
+        this.xp = xp;
+        isAlive = true;
+    }
 
     public int getDamage() {
         return damage;
@@ -34,6 +38,11 @@ public class Player {
         return name;
     }
 
+    /**
+     * Use this to see actual lvl
+     *
+     * @return actual lvl
+     */
     public int getLevel() {
         return level;
     }
@@ -42,27 +51,31 @@ public class Player {
         return health;
     }
 
-    public void setHealth(int health){
+    public void setHealth(int health) {
 
-         this.health = health;
+        this.health = health;
     }
 
     public void setXp(int xp) {
         this.xp = xp;
     }
 
-    public void addXp(int xp){
+    /**
+     * Adds XP to the player. If the new XP exceeds the threshold,
+     * the player automatically levels up. Any excess XP is retained.
+     *
+     * @param xp The amount of XP to add.
+     */
+    public void addXp(int xp) {
 
-         this.xp += xp;
-        // conectar o xp maximo com o setLevel
-        // level * 10 + 100
+        this.xp += xp;
         int xpToLeveling = getLevel() * 10 + 100;
-        if(getXp() > xpToLeveling){
+        if (getXp() > xpToLeveling) {
 
-            addLevel( 1 );
+            addLevel(1);
             // call setXp with exceeding xp
             int exceedingXp = getXp() - xpToLeveling;
-            if(exceedingXp > 0){
+            if (exceedingXp > 0) {
                 setXp(exceedingXp);
             }
             //TODO a better way to show the level up msg
@@ -73,10 +86,25 @@ public class Player {
 
     }
 
-    public void addLevel(int level){
+    public void addLevel(int level) {
 
         this.level += level;
+        this.health += 1;
+        if (level % 2 == 0) {
+            this.damage += 1;
+        }
 
+    }
+    /**Input the damage took
+     * @param damageTaken input any damage to be taken
+     */
+    public void tookDamage(int damageTaken){
+        int newHealth = getHealth() - damageTaken;
+        if(newHealth <= 0) {
+            isAlive = false;
+            System.out.println(getName() + " DIED!");
+        }
+        setHealth(newHealth);
     }
 
 }
