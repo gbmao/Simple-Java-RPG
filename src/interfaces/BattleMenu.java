286c9abd2da4c795c14e81e4a92b78faad4f5fa4
gbmao <gabriel.machado.oli@gmail.com>
2025-08-21 22:54:41 -0300

    feat: attack and battleOver methods

--- src/interfaces/BattleMenu.java
+++ src/interfaces/BattleMenu.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class BattleMenu {
 
-    public static void battleStart() {
+    public static String battleStart() {
 
         //iniciate enemies
 
@@ -23,14 +23,16 @@ public class BattleMenu {
            battleIsOver = battleMenu(enemy1);
 
         } while (!battleIsOver);
-        //TODO battle over msg and xp calculation
-        System.out.println("The battle is over!");
+
+        battleOver(enemy1);
+
+        return "\n\n You walk away from the dead enemy towards a new room\n\n ";
     }
 
     public static boolean battleMenu(Enemies enemies) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("You're facing an " + enemies.getEnemyType() + " with " + enemies.getHealth());
+        System.out.println("You're facing an " + enemies.getEnemyType() + " with " + enemies.getHealth() + " HP");
         System.out.println("You have : " + Player.player1.getHealth() + " HP");
         System.out.println("What you want to do?\n");
 
@@ -41,12 +43,8 @@ public class BattleMenu {
 
 
         switch (scanner.nextInt()) {
-            case 1:
-                enemies.tookDamage(Player.player1.getDamage());
-                if(!enemies.isAlive()){
-                    return true;
-                }
-                break;
+            case 1: return attackTurn(enemies);
+
             case 2:
                 System.out.println("2.Magic(not implemented)");
                 break;
@@ -63,6 +61,34 @@ public class BattleMenu {
 
         return false;
         //enemy attacks
+    }
+
+    public static boolean attackTurn(Enemies enemies){
+
+        System.out.println(Player.player1.getName() + " ATTACKS! " );
+        System.out.println(enemies.getEnemyType() + " took " + Player.player1.getDamage() + " damage\n");
+        enemies.tookDamage(Player.player1.getDamage());
+        if(!enemies.isAlive()) return true;
+
+        System.out.println(enemies.getEnemyType() + " ATTACKS! " );
+        System.out.println(Player.player1.getName() + " took " + enemies.getAttackDamage()  + " damage\n");
+        Player.player1.tookDamage(enemies.getAttackDamage());
+        if (!Player.player1.isAlive()) return true;
+        return false;
+    }
+
+    public static void battleOver(Enemies enemies){
+
+        System.out.println("\nThe battle is over!");
+        if (!enemies.isAlive()) {
+            System.out.println(enemies.getEnemyType() + " Died and gave : " +
+                    enemies.getXp() + " XP");
+            Player.player1.addXp(enemies.getXp());
+        } else if (!Player.player1.isAlive()) {
+            System.out.println("YOU DIED");
+
+        }
+
     }
 
 
