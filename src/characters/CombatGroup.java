143d86c9f02d0b654152d2304888b31f8171d49f
gbmao <gabriel.machado.oli@gmail.com>
2025-08-24 12:49:59 -0300

    wip: created dropItem - it drops itens (duh) atack and specialAttack methods call it for atack and a special atack( each subclass has one) whatToDo method call it to random chose normal attack or specialAttack

---
+++ src/characters/CombatGroup.java
@@ -0,0 +1,10 @@
+package characters;
+
+import database.GameItems;
+
+public class CombatGroup {
+   public  Slime slime = new Slime();
+    public  Goblin goblin = new Goblin();
+    public SkeletonArcher skeletonArcher = new SkeletonArcher();
+
+}
--- src/characters/Enemies.java
+++ src/characters/Enemies.java
@@ -1,10 +1,6 @@
 package characters;
-
 import database.GameItems;
-import items.HealingPotions;
-import items.ItemDrop;
 import items.Items;
-import items.OffensePotions;
 import utils.Utils;
 
 public class Enemies {
@@ -13,15 +9,17 @@ public class Enemies {
     private int attackDamage;
     private int xp;
     protected boolean isAlive;
+    private Items item;
+    private double dropRate;
 
-    public Enemies() {
-    }
 
-    public Enemies(String enemyType, int health, int attackDamage, int xp) {
+    public Enemies(String enemyType, int health, int attackDamage, int xp, Items item, double dropRate) {
         this.enemyType = enemyType;
         this.health = health;
         this.attackDamage = attackDamage;
         this.xp = xp;
+        this.item = item;
+        this.dropRate = dropRate;
         isAlive = true;
     }
 
@@ -41,6 +39,12 @@ public class Enemies {
         return attackDamage;
     }
 
+    public int attack() {
+        System.out.println(getEnemyType() + " attacks!!!");
+        return getAttackDamage();
+
+    }
+
     public int getXp() {
         return xp;
     }
@@ -58,36 +62,49 @@ public class Enemies {
     }
 
 
-    public void tookDamage(int damageTaken){
+    public void tookDamage(int damageTaken) {
         int newHealth = getHealth() - damageTaken;
-        if(newHealth <= 0) {
+        System.out.println(getEnemyType() + " took " + damageTaken + " damage!");
+        if (newHealth <= 0) {
             isAlive = false;
             System.out.println(getEnemyType() + " DIED!");
         }
         setHealth(newHealth);
     }
 
+    public void dropItem() {
+        Utils.dropRandomItem(item, dropRate);
+
+    }
+    public int whatToDo(){
+        double chance = Math.random();
+
+        if(chance < 0.6){
+            return attack();
+        }else{
+            return specialAttack();
+        }
+    }
+
+    public int specialAttack() {
+
+        return attack();
+    }
+
+
+
 }
 
-class  Slime extends Enemies {
+class Slime extends Enemies {
 
-    private Items item;
+    public Slime() {
+        super("Slime", 5, 1, 30, GameItems.hpPotion, 0.4);
 
-    public Slime(String enemyType, int health, int attackDamage, int xp, Items item) {
-        super(enemyType, health, attackDamage, xp);
-        this.item = item;
     }
 
-    public int Attack(){
-        return getAttackDamage();
-    }
-
-    public int specialAttack(){
+    public int specialAttack() {
+        System.out.println(getEnemyType() + " uses a SPECIAL move!");
         return Math.round(getAttackDamage() * (float) 1.5);
-    }
-
-    public void dropItem(){
-        ItemDrop.dropRandomItem(item, 0.8);
     }
 
 
@@ -95,11 +112,27 @@ class  Slime extends Enemies {
 }
 
 class Goblin extends Enemies {
-    public Goblin(String enemyType, int health, int attackDamage, int xp) {
-        super(enemyType, health, attackDamage, xp);
+
+    public Goblin() {
+        super("Goblin warrior", 14, 4, 60, GameItems.hpPotion, 0.6);
     }
 
-    public void goblinMagic(){
+//    public void specialAttack() {
+//        System.out.println(getEnemyType() + " starts concentration it's force!!!");
+//        setAttackDamage(getAttackDamage() + 1);
+//    }
 
+}
+
+class SkeletonArcher extends Enemies {
+
+    public SkeletonArcher() {
+        super("Skeleton Archer", 10, 5, 100, GameItems.poisonPotion, 0.5);
     }
+
+//    public void specialAttack() {
+//        System.out.println(getEnemyType() + " uses double hit!!!");
+////        attack(Player.player1);
+////        attack(Player.player1);
+//    }
 }
